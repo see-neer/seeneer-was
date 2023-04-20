4da2c632e1b251ad2f57a64a7d681d70f5e5d12c
Earth-KOR <lsk941017@naver.com>
2023-04-20 23:24:26 +0900

    Error Slack Alter and Request Response log add

--- src/main/java/com/repill/was/global/config/WebClientFactory.java
+++ src/main/java/com/repill/was/global/config/WebClientFactory.java
@@ -106,10 +106,6 @@ public class WebClientFactory {
     }
 
     public enum ServiceDestination {
-<<<<<<< Updated upstream
-        PUBLIC_DATA
-=======
-        AGGREGATION, SLACK
->>>>>>> Stashed changes
+        PUBLIC_DATA, SLACK
     }
 }
