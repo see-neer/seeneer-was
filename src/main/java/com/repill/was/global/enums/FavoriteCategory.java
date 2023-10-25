e5f37c319844738b76805b69ff3fcf8970335906
Earth-KOR <lsk941017@naver.com>
2023-10-25 19:14:06 +0900

    category 정보 추가

--- src/main/java/com/repill/was/global/enums/FavoriteCategory.java
+++ src/main/java/com/repill/was/global/enums/FavoriteCategory.java
@@ -9,23 +9,23 @@ import java.util.stream.Collectors;
 
 public enum FavoriteCategory {
     DAILY_LIFE(Map.of(Locale.ENGLISH, "DAILY_LIFE",Locale.KOREA, "일상생활"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_A_day_at_the_park_re_9kxj.png")),
     EXERCISE(Map.of(Locale.ENGLISH, "EXERCISE",Locale.KOREA, "운동"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_playing_golf_sdt3.png")),
     HEALTH(Map.of(Locale.ENGLISH, "HEALTH",Locale.KOREA, "건강"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_medicine_b1ol.png")),
     FINANCIAL_TECHNOLOGY(Map.of(Locale.ENGLISH, "FINANCIAL_TECHNOLOGY",Locale.KOREA, "재태크"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_Savings_re_eq4w.png")),
     HOBBY(Map.of(Locale.ENGLISH, "HOBBY",Locale.KOREA, "취미(문화생활)"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_Cooking_p7m1.png")),
     TRAVEL(Map.of(Locale.ENGLISH, "TRAVEL",Locale.KOREA, "여행"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_Aircraft_re_m05i.png")),
     RESTAURANT_CAFE(Map.of(Locale.ENGLISH, "RESTAURANT_CAFE",Locale.KOREA, "레스토랑/카페"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_breakfast_psiw.png")),
     SHOPPING(Map.of(Locale.ENGLISH, "SHOPPING",Locale.KOREA, "쇼핑"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, "")),
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_shopping_app_flsj.png")),
     COMMUNITY(Map.of(Locale.ENGLISH, "COMMUNITY",Locale.KOREA, "커뮤니티"),
-            Map.of(Locale.ENGLISH, "",Locale.KOREA, ""))
+            Map.of(Locale.ENGLISH, "",Locale.KOREA, "https://repill-dev.s3.ap-northeast-2.amazonaws.com/undraw_People_re_8spw.png"))
     ;
 
     private Map<Locale, String> displayText;
--- src/main/java/com/repill/was/operation/controller/dto/response/FavoriteCategoryResponse.java
+++ src/main/java/com/repill/was/operation/controller/dto/response/FavoriteCategoryResponse.java
@@ -16,13 +16,13 @@ public class FavoriteCategoryResponse {
     @ApiModelProperty(required = true, notes = "사유 표기용 text")
     private String displayText;
     @Deprecated
-    @ApiModelProperty(required = true, notes = "Deprecated 사유에 해당하는 탈퇴 방지 제안")
-    private String suggestion;
+    @ApiModelProperty(required = true, notes = "이미지 정보")
+    private String imageSrc;
 
-    public FavoriteCategoryResponse(String code, String displayText, String suggestion) {
+    public FavoriteCategoryResponse(String code, String displayText, String imageSrc) {
         this.code = code;
         this.displayText = displayText;
-        this.suggestion = suggestion;
+        this.imageSrc = imageSrc;
     }
 
     public static FavoriteCategoryResponse from(FavoriteCategory category, Locale locale) {
